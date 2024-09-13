641a51d2cc3afd8bed0c1d3d2f3ce89bbf1be32a
river3015 <aaxy8402@yahoo.co.jp>
2024-09-13 12:24:51 +0900

    modify: process_check.sh

--- shellscripts/process_check.sh
+++ shellscripts/process_check.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
+set -eu
 
-process_counts=$(pgrep -a nginx | wc -l)
+PROCESS_COUNTS=$(pgrep -a nginx | wc -l)
 
-if [ $process_counts -gt 1 ]; then
-        echo 'nginx process 1'
-        echo 'nginx is running'
-else
-        echo 'nginx process 0'
-        echo 'nginx is not running'
+if [ ${PROCESS_COUNTS} = 0 ]; then
+        echo nginx process ${PROCESS_COUNTS}
+        echo nginx is not running
         sudo systemctl start nginx
 
         # nginxが起動するまで待機
         sleep 5
-
-        check=$(systemctl is-active nginx)
-
-        # nginxが起動しているか確認
-        if [ $check = 'active' ]; then
-                echo "nginx is running"
-        fi
+	echo nginx is running
+else
+        echo nginx process ${PROCESS_COUNTS}
+        echo nginx is running
 fi
